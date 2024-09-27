5044abc7a124a4ccacf91348811ddb73947d81d8
perencine <55901404+perencine@users.noreply.github.com>
2024-09-26 23:42:54 -0300

    base_historica_tempo.py

--- base_historica_tempo.py
+++ base_historica_tempo.py
@@ -17,8 +17,11 @@ for year in range(five_years_ago.year, today.year + 1):
     print(f"Coletando dados de {start_date} a {end_date}...")
     url = f"https://api.weatherbit.io/v2.0/history/daily?start_date={start_date}&end_date={end_date}&lat={lat}&lon={lon}&key={api_key}"
     response = requests.get(url)
-    data = response.json()
-    weather_data.extend(data['data'])
+    if response.status_code == 200:
+        data = response.json()
+        weather_data.extend(data['data'])
+    else:
+        print(f"Erro: {response.status_code}")
         
 df = pd.DataFrame(weather_data)
 
