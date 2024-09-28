5b954461eaebd7459988b62933fb932d6d9616be
perencine <55901404+perencine@users.noreply.github.com>
2024-09-28 01:11:10 -0300

    Update previsao_diaria.py

--- codigos/previsao_diaria.py
+++ codigos/previsao_diaria.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 import time
 from datetime import datetime
+
 api_key = "54f03b5db6154b6fb5fb7fbed66772c6"
 lat = "-23.68"
 lon = "46.46"
@@ -9,7 +10,7 @@ url = f"https://api.weatherbit.io/v2.0/current?lat={lat}&lon={lon}&key={api_key}
 cols_interesse = ['clouds', 'datetime', 'dewpt', 'dhi', 'dni', 'ghi', 'precip', 'pres', 'rh', 'slp', 'solar_rad', 'temp', 'ts', 'wind_dir', 'wind_spd']
 arquivo = 'tehcchallenge3/arquivos/weather - reducao colunas.csv'
 
-
+# três tentativas de contato com a API
 for tentativas in range(3):
     response = requests.get(url)
     if response.status_code == 200:
@@ -20,7 +21,8 @@ for tentativas in range(3):
             if key in cols_interesse: new_line[key] = vlr
         if len(cols_interesse) == len(new_line):  # se veio todos os campos da lista cols_interesse, sucesso
             df_weather = pd.read_csv(arquivo)
-            df_weather = pd.concat([df_weather, pd.DataFrame([new_line])], ignore_index=True)
-            df_weather.to_csv(arquivo, index=False)
+            nova_condicao = pd.DataFrame([new_line])
+            df_weather_completo = pd.concat([df_weather, nova_condicao], ignore_index=True)
+            df_weather_completo.to_csv(arquivo, index=False)
             break
     time.sleep(3)
